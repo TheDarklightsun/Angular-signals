3d25a8a3bfd1938fc4689ea910b6b8438aa15ca9
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-11-20 03:28:05 +0200

    add-signalsBasic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import {DefaultComponent} from "./default/default.component";
+import {SignalsComponent} from "./signals/signals.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [DefaultComponent]
+  imports: [DefaultComponent, SignalsComponent]
 })
 export class AppComponent {
 
